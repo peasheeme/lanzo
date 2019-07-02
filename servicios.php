ba9950900ef6aa963ed9d99e0e01dd454fcd0d0a
perla <peasheeme@gmail.com>
2019-07-02 08:57:32 -0500

    Lanzo webpage

---
+++ servicios.php
@@ -0,0 +1,202 @@
+<?php require_once('includes/header.php') ?>
+		<!-- Intro section start -->
+		<section id="seguridad" class="section" >
+			<div class="container">
+				<div class="row text-left">
+					<div class="col-md-10">
+						<div class=" wow bounceInDown text-left">
+							<h2 class="mt-50">Servicios</h2>
+							<p class="white mt-20">Realizamos estudios e investigaciónes sobre conceptos contemplados por las normas de información financieras que son sujetos de Valuación y producen Utilidades, Beneficios Financieros, Administrativos y Fiscales.</p>
+							<a href="contacto.php"><button class="botones">Contáctanos</button></a>
+						</div>
+						<div class="col-md-2  "></div>
+					</div>
+				</div>
+			</div><!-- .row -->
+		</section>
+
+		<!-- Intro section end -->
+		<section id="about" class="section">
+			<div class="container">
+				<div class="row">
+						<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+								<div class="service">
+									<div class="icon">
+										
+									</div><br>
+									<h4> <img src="assets/images/icons/1.png" alt="Valuación de activos intangibles" width="70" style="padding-right: 20px;"> Valuación de activos intangibles</h4>
+									<div class="text-justify">
+											<p class="min"> Monitorea tu casa desde cualquier parte del mundo. Entérate de lo que está pasando en tu hogar, aunque te encuentres lejos.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+									<div class="service">
+										<div class="icon">
+											
+										</div><br>
+										<h4> <img src="assets/images/icons/1.png" alt="Valuación de activos intangibles" width="70" style="padding-right: 20px;"> Asesoría Jurídica Propiedad Intelectual</h4>
+										<div class="text-justify">
+												<p class="min"> Monitorea tu casa desde cualquier parte del mundo. Entérate de lo que está pasando en tu hogar, aunque te encuentres lejos.</p>
+										</div>
+									</div>
+								</div>
+					<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+						<div class="service">
+							<div class="icon">
+								
+							</div><br>
+							<h4> <img src="assets/images/icons/1.png" width="70" style="padding-right: 20px;" alt="Signos Distintivos"> Signos Distintivos</h4>
+							<div class="text-justify ">
+								<p class="min">Contamos con la experiencia de proteger con éxito las marcas de nuestros clientes tanto en México como en el extranjero. 
+									Tenemos Claro Que en el actual clima mundial y competitivo, los dueños de las marcas deben máximizar y coordinar la protección de estos activos en cualquier lugar del mundo, 
+								e integrarlos con actividades comerciales y planes. </p>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+							<div class="service">
+								<div class="icon">
+									
+								</div><br>
+								<h4> <img src="assets/images/icons/2.png" width="70" style="padding-right: 20px;" alt="Invenciones"> Invenciones</h4>
+								<div class="text-justify">
+										<p class="min">Nuestra actividad en temas de invenciones ha sido en una gran variedad de industrias, incluyendo
+											 la industra farmacéutica, química, petroquímica, energía, manufactura, sistemas de información, electrónica, eléctrica y mecánica.
+									
+										</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+								<div class="service">
+									<div class="icon">
+										
+									</div><br>
+									<h4> <img src="assets/images/icons/1.png" width="70" style="padding-right: 20px;"alt="Derechos de Autor"> Derechos de Autor</h4>
+									<div class="text-justify">
+											<p class="min"> La filosofía de LANZO CONSULTORES en este campo, no estriba en únicamente registrar, sino en construir derechos 
+												que le permitan a sus autores y titulares un seguro  de los mismos. Nuestra preocupación esta en definir 
+												apropiadas estrategias de aseguramiento de los
+												derechos ubicados en la ley de la materia, muchas veces complementados con figuras afines contenidos en leyes diversas, obteniendo así,
+												en algunos casos, un esquema de doble protección en favor de nuestros clientes.
+
+
+											</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+									<div class="service">
+										<div class="icon">
+											
+										</div><br>
+										<h4> <img src="assets/images/icons/2.png" width="70" style="padding-right: 20px;" alt="Nuevas Tecnologías"> Nuevas Tecnologías</h4>
+										<div class="text-justify">
+												<p class="min">
+													En LANZO CONSULTORES ofrecemos respaldo en materia de seguridad de datos o privacidad. Soportando en leyestales como la
+													 Ley Federal de Protección de Datos Personales en Posesión de los Particulares 
+													y otras normas que regulan precisamente el uso de las tecnologías de la informaciónen la red de redes, el temor de revelar
+													documentación privada, números de cuentas, propiedas, etc.
+												</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+										<div class="service">
+											<div class="icon">
+												
+											</div><br>
+											<h4> <img src="assets/images/icons/1.png"width="70" style="padding-right: 20px;"  alt="Litigio y cumplimiento"> Litigio y cumplimiento</h4>
+											<div class="text-justify">
+													<p class="min">Monitorea tu casa desde cualquier parte del mundo. Entérate de lo que está pasando en tu hogar, aunque te encuentres lejos.</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+											<div class="service">
+												<div class="icon">
+													
+												</div><br>
+												<h4> <img src="assets/images/icons/2.png" width="70" style="padding-right: 20px;" alt="Derecho a la imagen"> Derecho a la imagen</h4>
+												<div class="text-justify">
+														<p class="min">Monitorea tu casa desde cualquier parte del mundo. Entérate de lo que está pasando en tu hogar, aunque te encuentres lejos.</p>
+												</div>
+											</div>
+										</div>
+										<div class="col-xs-12 col-sm-6  col-lg-6 wow bounceInLeft">
+												<div class="service">
+													<div class="icon">
+														
+													</div><br>
+													<h4> <img src="assets/images/icons/2.png" width="70" style="padding-right: 20px;" alt="Nombres de Dominio" > Nombres de Dominio</h4>
+													<div class="text-justify">
+															<p class="min">Monitorea tu casa desde cualquier parte del mundo. Entérate de lo que está pasando en tu hogar, aunque te encuentres lejos.</p>
+													</div>
+												</div>
+											</div>
+											
+					
+
+				</div><!-- .row -->
+			</div><!-- .container -->
+		</section>
+
+		<section id="seguridad2" class="section-header">
+			<div class="color-overlay">
+				<div class="container">
+					<div class="row">
+
+						<div class="col-sm-12 wow bounceInDown text-left">
+							<h2>Beneficios</h2>
+							<div class="row">
+								<div class="col-xs-12 col-md-7">
+										<p class="blanco"> Realizamos estudios e investigaciónes sobre conceptos contemplados
+											 por las normas de información financieras que son sujetos de Valuación y producen beneficios como:</p>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	<section class="section">
+		<div class="container">
+			<div class="row dispMovil">
+
+			<div class="col-xs-12 col-sm-6  col-lg-3 text-center">
+						<img src="assets/images/icons/1.png" width="80" >
+						<div class="vertical-service mt-20 ">
+							<h4>Utilidades</h4>
+						</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-6  col-lg-3 text-center">
+						<img src="assets/images/icons/1.png" width="80" >
+						<div class="vertical-service mt-20 ">
+							<h4>Financieros</h4>
+							</div>
+				</div>
+				<div class="col-xs-12 col-sm-6  col-lg-3 text-center">
+						<img src="assets/images/icons/2.png" width="80" >
+						<div class="vertical-service mt-20 ">
+							<h4>Administrativos</h4>
+						</div>
+				</div>
+				<div class="col-xs-12 col-sm-6  col-lg-3 text-center">
+						<img src="assets/images/icons/1.png" width="80" >
+						<div class="vertical-service mt-20 ">
+							<h4>Fiscales</h4>
+						</div>
+				</div>
+
+			</div> <br><br>
+			
+
+				</div><!-- container--->
+			</section>
+
+
+			<?php require_once('includes/footer.php') ?>
